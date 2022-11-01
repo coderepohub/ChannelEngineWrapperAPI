9ba37dcffa197b99e77a43e4d938fdbf51bc5c18
coderepohub <amanchoudhary.com@gmail.com>
2022-11-01 13:14:33 +0530

    Created a blank ChannelEngine Client Console App.

---
+++ src/Clients/ChannelEngine.Clinets.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChannelEngine.Clinets.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
