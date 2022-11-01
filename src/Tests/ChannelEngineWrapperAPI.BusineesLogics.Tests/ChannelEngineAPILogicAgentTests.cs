a5b145858c2f32ba9e48bc2237f7860748363e3d
coderepohub <amanchoudhary.com@gmail.com>
2022-11-01 13:28:28 +0530

    add a blank unit test project for writing business logics unit test.

---
+++ src/Tests/ChannelEngineWrapperAPI.BusineesLogics.Tests/ChannelEngineAPILogicAgentTests.cs
@@ -0,0 +1,11 @@
+namespace ChannelEngineWrapperAPI.BusineesLogics.Tests
+{
+    [TestClass]
+    public class ChannelEngineAPILogicAgentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
